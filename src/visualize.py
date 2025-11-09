900faf2392da302f26befb846c72e8f0dcc752d8
sc <sau.mia206@icloud.com>
2025-11-08 16:59:27 -0800

    added axes to visualizations

--- src/visualize.py
+++ src/visualize.py
@@ -8,13 +8,12 @@ def visualization(filename, coords, route, distance):
     y = [p[1] for p in route_coords]
 
     # ensures the smaller side = 1920 pixels
-    width, height = 16, 12 
+    width, height = 12, 12 
     dpi = 1920 / min(width, height) 
     plt.figure(figsize=(width, height), dpi=dpi)
 
     plt.plot(x, y, 'o-') # plots each point with a dot
     plt.plot(x[0], y[0], 'ro')  # to mark charger location in red
-    plt.axis('off')
 
     img_file = f"{filename}_SOLUTION_{distance}.jpeg"
     plt.savefig(img_file)
